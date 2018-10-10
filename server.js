a7c9641ccb25f8b0708aaca888ea5d95d6a270c0
Christoper <celoranta@gmail.com>
2018-10-10 13:31:16 -0700

    Fix UTF encoding and add inactive header.html & style.css

--- server.js
+++ server.js
@@ -2,18 +2,18 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+//const mysql = require('mysql');
+
 var port = process.env.PORT || 8000;
 var hostname = "localhost";
 
 // const express = require('express');
 // const path = require('path')
 
-
 const server = http.createServer(function(req, res) 
 {
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
-
     fs.readFile(filename, function(err, data) {
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
@@ -21,9 +21,9 @@ const server = http.createServer(function(req, res)
           } 
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
+        res.write(q.pathname);
         res.end();
       });
-
 });
 
 server.listen(port, hostname, function() {
