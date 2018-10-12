e72ba5adb3c748d6d69a0d4ffa5b95de371e37eb
Christoper <celoranta@gmail.com>
2018-10-12 09:13:38 -0700

    Employ iteration on band member objects, replace slideshow images with our own

--- public/controllers/band-members.js
+++ public/controllers/band-members.js
@@ -1,33 +1,67 @@
-function catName() {
+function spaceCat() {
     return arguments[0].givenName + " " + arguments[0].surname
 };
 
-const chris = {
+function commaCat() {
+    var stringArray = arguments[0];
+    var returnString = "";
+    var i;
+    var lastIndex = 0 ? 0 : stringArray.length - 1;
+    for (i = 0; i < stringArray.length; i++) {
+        returnString += stringArray[i];
+        if (i != lastIndex) {
+            returnString += ", "
+        }
+    };
+    return returnString;
+};
+
+const chrisEloranta = {
     givenName: 'Chris',
     surname: 'Eloranta',
     instruments: ['Bass', 'Voice'],
     headShots: ['/images/chris_grin.jpg'],
-    fullName: function() {return catName(this)}
+    fullName: function() {return spaceCat(this)},
+    instrList: function() {
+        return commaCat(this.instruments);
+    }
 };
 
-const brad = {
+const bradRoss = {
     givenName: 'Brad',
     surname: 'Ross',
     instruments: ['Drums', 'Voice'],
     headShots: ['/images/brad_lebowsky.jpg'],
-    fullName: function() {return catName(this)}
+    fullName: function() {return spaceCat(this)},
+    instrList: function() {
+        return commaCat(this.instruments);
+    }
 };
 
-const lorne = {
+const lorneBaron = {
     givenName: 'Lorne',
     surname: 'Baron',
     instruments: ['Guitar', 'Voice'],
-    headShots: ['/images/lorne_hat.PNG'],
-    fullName: function() {return catName(this)}
+    headShots: ["/images/lorne_hat.PNG"],
+    fullName: function() {return spaceCat(this)},
+    instrList: function() {
+        return commaCat(this.instruments);
+    }
 };
 
-const bandMembers = [lorne, brad, chris];
+const bandMembers = [lorneBaron, bradRoss, chrisEloranta];
 
-document.getElementById("band-member-name-1").innerHTML = bandMembers[0].fullName();
-document.getElementById("band-member-name-2").innerHTML = bandMembers[1].fullName();
-document.getElementById("band-member-name-3").innerHTML = bandMembers[2].fullName();
+i = 0;
+for (i = 0; i < bandMembers.length; i++) {
+    var bandMember = bandMembers[i];
+    var memberString = "band-member-" + i + "-";
+    var nameString = memberString + "name";
+    var headshotString = memberString + "headshot";
+    var instrumentsString = memberString + "instruments";
+
+    document.getElementById(nameString).innerHTML = bandMembers[i].fullName();
+    document.getElementById(headshotString).setAttribute('src', bandMembers[i].headShots[0]);
+    document.getElementById(instrumentsString).innerHTML = bandMembers[i].instrList();
+}
+
+
