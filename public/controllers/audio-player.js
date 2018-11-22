bbb7c1ddfa087652c50e99ec4b096c486c97db1c
Christoper <celoranta@gmail.com>
2018-11-22 11:47:43 -0800

    failed attempt to randomize recordings from flip-flopping halves

--- public/controllers/audio-player.js
+++ public/controllers/audio-player.js
@@ -34,13 +34,87 @@ const recordings = [
 //   Your browser does not support the audio element.
 // </audio>
 
+var activeArray = shuffle(recordings);
+var activeArray = randomizeHalves(recordings);
+
+var stateModule = (function () {
+    var state; // Private Variable
+
+    var pub = {};// public object - returned at end of module
+
+    pub.changeState = function (newstate) {
+        state = newstate;
+    };
+
+    pub.getState = function() {
+        return state;
+    }
+    return pub; // expose externally
+}());
+
+function flipFlop(){
+    var state = stateModule.getState;
+    if (state = 0) {
+        stateModel.changeState = 1
+    }
+    else (
+        stateModel.changeState = 0
+    )
+    return stateModule.getState;
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  function splitArray(arrayToSplit) {
+    var arrayA = arrayToSplit.slice(0, Math.floor(arrayToSplit.length / 2));
+    var arrayB = arrayToSplit.slice(Math.ceil(arrayToSplit.length / 2));
+    return {'splits' : [{'A': arrayA}, {'B' : arrayB}]};
+  }
+
+function randomizeHalves(arrayToRandomize) {
+  var splitObject = splitArray(arrayToRandomize);
+  newArray = splitObject.A + splitObject.B;
+  return newArray;
+}
+
 function pauseAudio() {
     var x = document.getElementById("myAudio");
     x.pause();
 }
 
-function getRandomRecording() {
-    var index = Math.floor((Math.random() * recordings.length));
-    return recordings[index].url;
+function chooseAtRandom(arrayToChooseFrom) {
+var index = Math.floor((Math.random() * arrayToChooseFrom.length));
+return arrayToChooseFrom[index];
 }
+
+function getRandomRecording() {
+    const state = flipFlop();
+    var half;
+    if (state = 0) {
+        half = 'A';
+    }
+    else {
+        half = 'B'
+    }
+    const activeHalf = 
+    return half;
+}
+
+stateModule.changeState(0);
 
