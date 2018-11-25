b59ddf565678424abe2ca25760ede1b27793a25d
Christoper <celoranta@gmail.com>
2018-11-25 01:57:01 -0800

    Video is playing and layered over content

--- public/controllers/splash-video.js
+++ public/controllers/splash-video.js
@@ -8,20 +8,27 @@ function setOpacity(element, opacityValue) {
 element.style.opacity = opacityValue;
 }
 
-function fade(fadingElement){
+function fadeAndHide(fadingElement){
     const vidDuration = fadingElement.duration;
     const timeRemaining = vidDuration - fadingElement.currentTime;
     if (timeRemaining < 1){
         setOpacity(fadingElement, timeRemaining);
+        if (timeRemaining == 0) {
+            fadingElement.style.display = 'none';
+        }
     }
 }
 
-var splashVideoDiv = document.getElementById('splash-video');
+// var splashVideoDiv = document.getElementById('splash-video');
 splashVideoMediaObject = getVideo();
 splashVideoMedia = splashVideoMediaObject.source;
+
+splashVideoDiv = document.getElementById('splash-video')
 splashVideoDiv.setAttribute('src', splashVideoMedia);
-splashVideoDiv.addEventListener('timeupdate', function(){fade(splashVideoDiv)})
-
+splashVideoDiv.addEventListener('timeupdate', function(){fadeAndHide(splashVideoDiv)});
+splashVideoDiv.setAttribute('style', 'width:100%; height: auto');
+// splashVideoDiv.setAttribute('position', 'absolute');
+splashVideoDiv.muted = true;
 setOpacity(splashVideoDiv, 1);
-
 splashVideoDiv.play();
+// splashVideoDiv.addEventListener(onended, function(){container.remove()})
