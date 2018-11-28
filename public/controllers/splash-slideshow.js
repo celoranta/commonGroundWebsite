ec0282c111efc544ae07d80378df69cdac67144e
Christoper <celoranta@gmail.com>
2018-11-27 23:14:40 -0800

    Fire splash carousel onload

--- public/controllers/splash-slideshow.js
+++ public/controllers/splash-slideshow.js
@@ -1,6 +1,6 @@
 function getSplashSlides() {
     splashSlideData = [
-        {"source" : "/images/black.jpg"},
+        { "source": "/images/black.jpg" },
         { "source": "/images/BassFrame.jpg" },
         { "source": "/images/DrumFrame.jpg" },
         { "source": "/images/TeleFrame.jpg" },
@@ -12,19 +12,13 @@ function getSplashSlides() {
 var divsToHide = document.getElementsByClassName('hide-during-splash');
 console.log("Divs to hide qty: " + divsToHide.length);
 i = 0;
-for (i = 0; i < divsToHide.length; i++)
-{
+for (i = 0; i < divsToHide.length; i++) {
     const divToHide = divsToHide[i];
     divToHide.style.display = 'none';
 }
 var divsToShowBeforeFade = document.getElementsByClassName('hide-during-video');
-// splashVideoMediaObject = getVideo();
-// splashVideoMedia = splashVideoMediaObject.source;
-
 var slides = getSplashSlides();
-
 var splashSlidesContainer = document.getElementById('splash-slides-container');
-
 
 i = 0;
 for (i = 0; i < slides.length; i++) {
@@ -35,7 +29,6 @@ for (i = 0; i < slides.length; i++) {
     slideImage.classList.add('slide-image', 'custom-greyscale')
     slideImage.setAttribute('style', "width:100%");
 
-    
     //Add slide image source
     const nextSlide = slides[i];
     const slideSource = nextSlide.source;
@@ -48,56 +41,62 @@ for (i = 0; i < slides.length; i++) {
 
 // Automatic Slideshow - change image every 0.7 seconds
 var myIndex = 0;
-splashCarousel();
+//splashCarousel();
 
 // Milliseconds
-function sleep(millisecs) {
-    var initiation = new Date().getTime();
-    while ((new Date().getTime() - initiation) < millisecs);
-}
+// function sleep(millisecs) {
+//     var initiation = new Date().getTime();
+//     while ((new Date().getTime() - initiation) < millisecs);
+// }
 
-function fadeAndHide(fadingElement, fadeDurationMs, hiddenCallback) {
-    const fadeFrameDuration = 100;
-    var opacity = 1;
-    i = 0;
-    for (i = fadeDurationMs; i > 0; i -= fadeFrameDuration) {
-        fadingElement.style.opacity = "0.25";
-        fadingElement.style.backgroundcolor = "blue";
-        sleep(fadeFrameDuration);
-    }
-    fadingElement.style.display = 'none';
-    hiddenCallback();
+// function fadeAndHide(fadingElement, fadeDurationMs, hiddenCallback) {
+//     const fadeFrameDuration = 100;
+//     var opacity = 1;
+//     i = 0;
+//     for (i = fadeDurationMs; i > 0; i -= fadeFrameDuration) {
+//         fadingElement.style.opacity = "0.25";
+//         fadingElement.style.backgroundcolor = "blue";
+//         sleep(fadeFrameDuration);
+//     }
+//     fadingElement.style.display = 'none';
+//     hiddenCallback();
 
-}
+// }
 
 function fade(element, callback) {
     var op = 1;  // initial opacity
     var timer = setInterval(function () {
-        if (op <= 0.01){
+        if (op <= 0.01) {
             clearInterval(timer);
             element.style.display = 'none';
-            if(callback){
-            callback();
+            if (callback) {
+                callback();
             }
         }
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        //Fade progressively faster as the image reaches full transparency
+        if (op <= 0.2){
+            op-= op * 0.02
+        }
+        //Fade progressively faster as the image reaches full transparency
+        if (op <= 0.1){
+            op-= op * 0.04
+        }
         op -= op * 0.02;
     }, 10);
 }
 
 function showDivs(divsToShow, callback) {
-    var i=0;
-    for (i = 0; i < divsToShow.length; i++)
-{
-    const divToShow = divsToShow[i];
-    divToShow.style.display = 'block';
+    var i = 0;
+    for (i = 0; i < divsToShow.length; i++) {
+        const divToShow = divsToShow[i];
+        divToShow.style.display = 'block';
+    }
+    if (callback) {
+        callback();
+    }
 }
-if (callback) {
-    callback();
-}
-}
-
 
 // splashVideoDiv, 
 // function(){showDivs(divsToShowBeforeFade)},
@@ -113,16 +112,16 @@ function splashCarousel() {
         mySplashSlides[i].style.display = "none";
     }
     if (myIndex < mySplashSlides.length) {
-    setTimeout(splashCarousel, 700);
-    myIndex++;
-    mySplashSlides[myIndex-1].style.display = "block";
+        setTimeout(splashCarousel, 700);
+        myIndex++;
+        mySplashSlides[myIndex - 1].style.display = "block";
     }
     else {
-        var lastSlide = mySplashSlides[myIndex-1];
+        var lastSlide = mySplashSlides[myIndex - 1];
         lastSlide.style.display = "block";
-        setTimeout(function(){
-        showDivs(divsToShowBeforeFade);
-        fade(lastSlide, function(){showDivs(divsToHide, function(){animateSongList()})});
+        setTimeout(function () {
+            showDivs(divsToShowBeforeFade);
+            fade(lastSlide, function () { showDivs(divsToHide, function () { animateSongList() }) });
         }, 700)
 
     }
