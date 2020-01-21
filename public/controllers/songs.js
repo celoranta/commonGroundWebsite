b8ac184b83eb6b4d26aca1f9852c2aa3c0a13b85
Christoper <celoranta@gmail.com>
2020-01-21 11:38:00 -0800

    Add tsv convert function

--- public/controllers/songs.js
+++ public/controllers/songs.js
@@ -273,5 +273,17 @@ window.requestAnimationFrame = (function(){
           requestAnimationFrame(songSlide);
   })();
 };
+
+function tsvJSON(tsv) {
+    const lines = tsv.split('\n');
+    const headers = lines.slice(0, 1)[0].split('\t');
+    return lines.slice(1, lines.length).map(line => {
+      const data = line.split('\t');
+      return headers.reduce((obj, nextKey, index) => {
+        obj[nextKey] = data[index];
+        return obj;
+      }, {});
+    });
+  };
   
 
